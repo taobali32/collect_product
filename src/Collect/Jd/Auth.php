2242f722eaad0cbf9bd0ba5b24606edcb91f9e70
lwl <3196672779@qq.com>
2021-04-13 23:56:40 +0800

    jd-京东授权

--- src/Collect/Jd/Auth.php
+++ src/Collect/Jd/Auth.php
@@ -10,44 +10,10 @@ class Auth extends BaseClient
 {
     /**
      * 京东授权
-     * @see https://www.ecapi.cn/index/index/openapi/id/57.shtml?ptype=2
-     */
-//    public function auth($param = [],$user_id)
-//    {
-//        $uri = 'http://api.web.21ds.cn/jingdong/createUnionPosition';
-//
-//        $config = $this->app['config']['jd'];
-//
-//        $defaultConfig = [
-//            'apkey' =>  $config['miao_you_quan']['apkey'],
-//            'key_id'    =>  $config['lian_meng']['key_id'],
-//            'unionType' =>  1,
-//            'type'  =>  2,
-//            'spaceNameList' =>  'jd_' . $user_id,
-//            'siteId'    =>   $config['lian_meng']['site_id'],
-//        ];
-//
-//        $mergeConfig = array_merge($defaultConfig,$param);
-//
-////        dd($mergeConfig);
-//
-//        $response = $this->httpGet($uri,$mergeConfig);
-//
-//        dd($response);
-//        if ($response['code'] == -1){
-//            throw new \Exception($response['msg'],$response['code']);
-//        }
-//
-//        return $response['data']['resultList']['jd_' . $user_id];
-//    }
-
-
-    /**
-     * auth
      * @see http://www.jingtuitui.com/api_item?id=20
      * @param array $param
      * @param $user_id
-     * @return mixed
+     * @return array
      * @throws \GuzzleHttp\Exception\GuzzleException
      * @throws \Gather\Kernel\Exceptions\InvalidConfigException
      */
@@ -58,25 +24,26 @@ class Auth extends BaseClient
         $config = $this->app['config']['jd'];
 
         $defaultConfig = [
-            'apkey' =>  $config['miao_you_quan']['apkey'],
-            'key_id'    =>  $config['lian_meng']['key_id'],
+            'appid' =>  $config['jing_tui_tui']['appid'],
+            'appkey'    =>  $config['jing_tui_tui']['appkey'],
+            'return_type' =>  'json',
+
+            'unionid'   =>  $config['lian_meng']['unionid'],
+            'key'       =>  $config['lian_meng']['key'],
             'unionType' =>  1,
-            'type'  =>  2,
-            'spaceNameList' =>  'jd_' . $user_id,
-            'siteId'    =>   $config['lian_meng']['site_id'],
+            'type'      =>  1,
+            'name'      =>  'jd_' . $user_id,
+            'siteId'    =>  $config['lian_meng']['siteId']
         ];
 
         $mergeConfig = array_merge($defaultConfig,$param);
 
-//        dd($mergeConfig);
-
         $response = $this->httpGet($uri,$mergeConfig);
 
-        dd($response);
-        if ($response['code'] == -1){
-            throw new \Exception($response['msg'],$response['code']);
+        if ($response['return'] != 0){
+            throw new \Exception($response['result'],$response['return']);
         }
 
-        return $response['data']['resultList']['jd_' . $user_id];
+        return $response['result'];
     }
 }
